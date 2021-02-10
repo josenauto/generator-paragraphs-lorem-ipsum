85b465083760a290a53aa697b9b862fed89a2cfb
josenauto <jemilionautoch@gmail.com>
2021-02-10 09:21:48 -0300

    Added useState, a form and two paragraphs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello world");
+  };
   return (
-    <h2>Lorem impsun project setup</h2>
+    <div>
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit">generate</button>
+      </form>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          similique in. Sed, esse rerum voluptate perferendis sint omnis,
+          repellat consequatur temporibus accusantium in facilis minima placeat
+          minus corporis aliquid iste.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+          similique in. Sed, esse rerum voluptate perferendis sint omnis,
+          repellat consequatur temporibus accusantium in facilis minima placeat
+          minus corporis aliquid iste.
+        </p>
+      </div>
+    </div>
   );
 }
 
