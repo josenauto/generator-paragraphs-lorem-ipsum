e02bc91aa72dc6ed03531d7a6860ff6f2c50b7ec
josenauto <jemilionautoch@gmail.com>
2021-02-15 17:02:39 -0300

    Added reactstrap and some custom styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React, { useState } from "react";
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input,
+  Form,
+} from "reactstrap";
 import data from "./data";
 
 function App() {
@@ -7,37 +17,43 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let amount = parseInt(count)
+    let amount = parseInt(count);
     if (count <= 0) {
-      amount = 1
+      amount = 1;
     }
     if (count > 8) {
-      amount = 9
+      amount = 9;
     }
-    setText(data.slice(0,amount))
+    setText(data.slice(0, amount));
   };
   return (
-    <div>
-      <h3>Tired of boring lorem ipsum?</h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="amount">paragraphs:</label>
-        <input
-          type="number"
-          name="amount"
-          id="amount"
-          value={count}
-          onChange={(e) => setCount(e.target.value)}
-        />
-        <button type="submit">generate</button>
-      </form>
-      <div>
-        {
-          text.map((item, index)=>{
-            return <p key={index}>{item}</p>
-          })
-        }
+    <Container>
+      <h1 className="text-center">Tired of boring lorem ipsum?</h1>
+      <div className="form-container">
+        <Form inline onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label htmlFor="amount">
+              Paragraphs:
+            </Label>
+            <Input
+              type="number"
+              name="amount"
+              id="amount"
+              value={count}
+              onChange={(e) => setCount(e.target.value)}
+            />
+          </FormGroup>
+          <Button color="primary" type="submit">
+            Generate
+          </Button>
+        </Form>
       </div>
-    </div>
+      <div className="text-container">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </div>
+    </Container>
   );
 }
 
